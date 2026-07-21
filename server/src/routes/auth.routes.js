8a8f459a1155ed08ee57152fe0e4605c469c8ab1
pranjalnikum31 <pranjalnikum31@gmail.com>
2026-07-21 20:57:03 +0530

    Implement role-based authorization (RBAC)

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,15 +1,24 @@
-const express=require('express');
-const { register,login } = require("../controllers/auth.controller");
-const router=express.Router();
-const {verifyUser} = require("../middleware/auth.middleware");
+const express = require("express");
+const { register, login } = require("../controllers/auth.controller");
+const router = express.Router();
+const { verifyUser } = require("../middleware/auth.middleware");
+const authorize = require("../middleware/role.middleware");
 
-router.post("/register",register);
-router.post("/login",login);
-router.get("/profile",verifyUser,(req,res)=>{
-    res.json({
-        success: true,
-        user:req.user
-    });
+router.post("/register", register);
+router.post("/login", login);
+router.get("/profile", verifyUser, (req, res) => {
+  res.json({
+    success: true,
+    user: req.user,
+  });
 });
 
-module.exports=router;
+router.get("/owner", verifyUser, authorize("OWNER"), (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome Owner!",
+    user: req.user,
+  });
+});
+
+module.exports = router;
